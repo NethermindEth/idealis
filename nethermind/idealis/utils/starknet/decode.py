c496523d9b5c0b6ed9bd646cf883817ce0476556
Eli Barbieri <elicbarbieri@gmail.com>
2024-07-11 16:22:30 -0700

    splitting starknet_getClass response parsing

--- nethermind/idealis/utils/starknet/decode.py
+++ nethermind/idealis/utils/starknet/decode.py
@@ -12,6 +12,23 @@ root_logger = logging.getLogger("nethermind")
 logger = root_logger.getChild("starknet").getChild("decoding")
 
 
+def get_parsed_class(class_hash, rpc_url) -> StarknetAbi | None:
+    class_abi = sync_get_class_abi(class_hash, rpc_url)
+    if isinstance(class_abi, str):
+        try:
+            class_abi = json.loads(class_abi)
+        except json.JSONDecodeError:
+            logger.error(f"Invalid ABI for class 0x{class_hash.hex()}.  Could not parse ABI JSON...")
+            return None
+    if class_abi is None or len(class_abi) == 0:
+        logger.warning(f"Empty ABI for class 0x{class_hash.hex()}.  Skipping...")
+        return None
+    try:
+        return StarknetAbi.from_json(abi_json=class_abi, class_hash=class_hash, abi_name="")
+    except BaseException as e:
+        logger.error(f"Error parsing ABI for class 0x{class_hash.hex()}...  {e}")
+
+
 class PessimisticDecoder(DecodingDispatcher):
     rpc_url: str
     """ Starknet RPC URL to use for fetching class ABIs """
@@ -33,31 +50,10 @@ class PessimisticDecoder(DecodingDispatcher):
         if class_id in self.invalid_class_ids:
             return None
 
-        class_abi = sync_get_class_abi(class_hash, self.rpc_url)
+        class_abi = get_parsed_class(class_hash, self.rpc_url)
         if class_abi is None:
             self.invalid_class_ids.add(class_id)
             return None
 
-        if isinstance(class_abi, str):
-            try:
-                class_abi = json.loads(class_abi)
-
-            except json.JSONDecodeError:
-                logger.error(f"Invalid ABI for class 0x{class_hash.hex()}.  Could not parse ABI JSON...")
-                self.invalid_class_ids.add(class_hash[-8:])
-                return None
-
-        if class_abi is None or len(class_abi) == 0:  # Lots of ABIs are empty with no members
-            logger.warning(f"Empty ABI for class 0x{class_hash.hex()}.  Skipping...")
-            self.invalid_class_ids.add(class_hash[-8:])
-            return None
-
-        try:
-            class_abi = StarknetAbi.from_json(abi_json=class_abi, class_hash=class_hash, abi_name="")
-            self.add_abi(class_abi)
-            return self.class_ids[class_id]
-
-        except BaseException as e:
-            logger.error(f"Error parsing ABI for class 0x{class_hash.hex()}...  {e}")
-            self.invalid_class_ids.add(class_id)
-            return None
+        self.add_abi(class_abi)
+        return self.class_ids[class_id]
